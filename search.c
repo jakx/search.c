9201c74c93ab386205c44f9316cb71336e4fd0bf
Jack Daines <dainesj@gmail.com>
2014-05-04 23:18:39 -0700

    added browser choice with -b option

--- search.c
+++ search.c
@@ -12,19 +12,31 @@ example: search how to grow a moss -- will open a firefox window with the query
 #include <string.h>
 #include <unistd.h>
 #include <stdio.h>
+#include <ctype.h>
 	
 int
 main(int argc, char *argv[])
 {
+        char *browser = "firefox";
+        int c;
+        int bflag = 0;;
+        while ((c = getopt (argc, argv, "b:")) != -1)
+           switch (c)
+           {
+           case 'b':
+             bflag = 1;
+             browser = optarg;
+             break;
+           }
         char buf[256]  = "http://www.google.com/search?q=";
-	snprintf(buf, sizeof buf, "%s", "http://www.google.com/search?q=");
+    	snprintf(buf, sizeof buf, "%s", "http://www.google.com/search?q=");
         int i = 0;
-        for(i = 1; i < argc; i++){
+        for(i = optind; i < argc; i++){
                strcat(buf, argv[i]);
                if(i+1 < argc)
 		       strcat(buf, " ");
         }
-        char *argt[] = {"firefox", buf, NULL};
-	execvp(argt[0], argt);
+        char *argt[] = {browser, buf, NULL};
+    	execvp(argt[0], argt);
 }
 
